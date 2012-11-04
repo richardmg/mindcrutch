3344d81b44ae6a039d5216bb215c9f50dca1ca8c
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2012-11-04 14:33:14 +0100

    supply userip to avoid throttle

--- wooimagepalette.js
+++ wooimagepalette.js
@@ -10,13 +10,15 @@ $.fn.fillWithGoogleImages = function(props, callback)
 
     for (var start=0; start<(7*rsz); start+=rsz) {
         var url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0" +
-            "&rsz=" + rsz + "&q=" + props.searchString + "&start=" + start + "&callback=?" + "&imgsz=" + props.imgsz +
-            "&imgtype=" + props.imgtype + "&imgcolor=" + props.imgcolor + "&imgc=" + props.imgc;
+            "&rsz=" + rsz + "&q=" + props.searchString + "&start=" + start + "&callback=?" +
+            "&imgsz=" + props.imgsz + "&imgtype=" + props.imgtype + "&imgcolor=" + props.imgcolor + "&imgc=" + props.imgc +
+            "&userip=84.208.230.46";
 
         $.getJSON(url, function(json) {
                 //console.log("Return:" + JSON.stringify(json));
                 $plugin_this.each(function() {
                     var view = $(this);
+                    console.log(json);
                     for (var i in json.responseData.results) {
                         var r = json.responseData.results[i];
                         var e = $("<div class='thumbnail'><img src='" + r.tbUrl + "'></img></div>");
